02b48c905d0a38299228e79f44a407704d2c2269
Maria <aerocodepro@gmail.com>
2021-12-09 22:50:15 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
     $('a.tab-header').click(function(e){
         e.preventDefault();
         activeBlock = $(this).closest('.tab-item');
+        //hide all blocks
         activeBlock.siblings().removeClass('active');
-        $('.tab-header, .tab-content').removeClass('active');
+        activeBlock.siblings().find('.tab-content').fadeOut(1000);
+        //show active block
         activeBlock.addClass('active');
-        activeBlock.find('.tab-header').addClass('active');
-        activeBlock.find('.tab-content').addClass('active');
+        activeBlock.find('.tab-content').fadeIn(1000);
     });
 });
